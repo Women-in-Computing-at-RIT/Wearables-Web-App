84ed986acb448e77200423147b17516c2e1060ee
Cara <cxs4754@rit.edu>
2016-06-07 15:04:25 -0400

    Added documentation at the top of information.jsx

--- imports/ui/pages/information.jsx
+++ imports/ui/pages/information.jsx
@@ -1,8 +1,11 @@
+/**
+ * file: information.jsx
+ * authors: Cara Steinberg
+ */
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 export const Info = () =>
   <Jumbotron className="text-center">
     <h2>Thank you for signing up!</h2>
-    <p>Lorem ipsum sit amet dolor asent</p>
   </Jumbotron>;
