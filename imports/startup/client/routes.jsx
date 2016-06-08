2e0b0265c48c4ebbc93f17917dee06210e76b3cc
Matthew Crocco <matthewcrocco@gmail.com>
2016-06-08 12:31:08 -0400

    Add meteor email package. Add client code to remove the IronRouter "No route found" message on web pages. Add VERY basic 404 page (was used for testing).

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -7,6 +7,7 @@ import {Meteor} from 'meteor/meteor';
 import {App} from '../../ui/layouts/app';
 import {Index} from '../../ui/pages/index';
 import {Info} from '../../ui/pages/information';
+import {NotFound} from '../../ui/pages/not-found';
 
 /**
  * An onEnter action that requires the user accessing that route to be authenticated. If the user is not authenticated
@@ -44,6 +45,22 @@ Meteor.startup(() => {
         <IndexRoute name="index" component={Index}/>
         <Route name="info" path="/info" component={Info}/>
       </Route>
+      <Route path="*" component={NotFound}/>
     </Router>
     , document.getElementById('react-root-container'));
+
+  // TODO Fix IronRouter error display or write own admin panel. The current admin panel depends on IR but we use RR
+  /*
+    Wait on a 20 ms interval for the iron router error to appear, once it appears it is removed
+    and the interval is cleared, ending the check.
+   */
+  let intervalId = setInterval(() => {
+    let nexts = $('#react-root-container').next('div');
+
+    if(nexts.length === 0)
+      return;
+
+    nexts.remove();
+    clearInterval(intervalId);
+  }, 20);
 });
---
+++ imports/ui/pages/not-found.jsx
@@ -0,0 +1,4 @@
+
+export const NotFound = () => {
+  return <h3>Not Found</h3>;
+};
