05fcb1ca4dc9f942d577794dcd52f830d12c4648
Cara <cxs4754@rit.edu>
2016-06-09 09:40:07 -0400

    Changed the information page to user render method.

--- imports/ui/pages/information.jsx
+++ imports/ui/pages/information.jsx
@@ -3,15 +3,28 @@
  * authors: Cara Steinberg
  */
 import React from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Jumbotron} from 'react-bootstrap';
 // import ReactAutoForm from 'meteor-react-autoform';
 
-export const Profile = () => (
-  <Row>
-    <Col xs={ 12 }>
-      <h2 className="page-header">Profile</h2>
-    </Col>
-  </Row>
-);
+export const Profile = React.createClass({
+  propTypes: {
+    profile: React.PropTypes.element.isRequired
+  },
+  render() {
+    return <div>
+      <Jumbotron className="text-center"><h3>Thank you for signing up!</h3></Jumbotron>
+      <Row>
+        <Col xs={12}>
+          <h2 className="page-header">Profile</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          { this.props.profile }
+        </Col>
+      </Row>
+    </div>;
+  }
+});
 
 
