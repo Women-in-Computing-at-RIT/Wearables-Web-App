125acb2488cf9a74d00d18de2c5bc98f28592025
Cara <cxs4754@rit.edu>
2016-06-13 16:59:29 -0400

    Edited forgot password page.

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -9,7 +9,7 @@ import {Index} from '../../ui/pages/index';
 import {Profile} from '../../ui/pages/information';
 import {NotFound} from '../../ui/pages/not-found';
 import {ContactPage} from '../../ui/pages/contact';
-import {ResetPassword} from '../../ui/pages/resetPassword';
+import {ForgotPassword} from '../../ui/pages/forgotPassword';
 
 /**
  * An onEnter action that requires the user accessing that route to be authenticated. If the user is not authenticated
@@ -47,7 +47,7 @@ Meteor.startup(() => {
         <IndexRoute name="index" component={Index}/>
         <Route name="info" path="/info" component={Profile}/>
         <Route name="contact" path="/contact" component={ContactPage}/>
-        <Route name="resetPassword" path="/forgotPassword" component={ResetPassword}/>
+        <Route name="resetPassword" path="/forgotPassword" component={ForgotPassword}/>
       </Route>
       <Route path="*" component={NotFound}/>
     </Router>
---
+++ imports/ui/pages/forgotPassword.jsx
@@ -0,0 +1,38 @@
+/**
+ * file: resetPassword.jsx
+ * authors: Cara Steinberg
+ */
+
+import React from 'react';
+import Schemas from '../../modules/schemas';
+import {Button, Row, Col, Form, FormGroup} from 'react-bootstrap';
+
+export const ForgotPassword = React.createClass ({
+  handleSubmit (e) {
+    e.preventDefault();
+    this.setState({submitted: this.refs.ProfileForm.getFormData()});
+    console.log("Submitted");
+  },
+  render() {
+    return (
+      <Row>
+        <Col xs={12}>
+          <h2 className="page-header">Forgot Password?</h2>
+        </Col>
+        <Col xs={12}>
+          <h4 className="text-left">
+            Please enter your email address, and we'll send you a link and instructions to reset your password.
+          </h4>
+          <FormGroup>
+            <Form schema={Schemas.User} id="signInForm" onsubmit={this.handleSubmit}>
+              <label htmlFor="email">Enter Email Address</label>
+              <input className="form-control" type="email" placeholder="Email" id="email"/>
+              <br/>
+              <Button type="submit" className="center-block">Submit</Button>
+            </Form>
+          </FormGroup>
+        </Col>
+      </Row>
+    );
+  }
+});
